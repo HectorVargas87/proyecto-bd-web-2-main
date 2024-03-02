467af8c576d9b7bcba5fa40fd6ceb886cfc26afe
HectorVargas87 <113728891+HectorVargas87@users.noreply.github.com>
2024-03-01 22:34:33 -0600

    Update user.route.ts

--- proyecto-bd-web-2-main/src/routes/user.route.ts
+++ proyecto-bd-web-2-main/src/routes/user.route.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { User } from '../types/user.type'
+import { User, UserModel } from '../types/user.type'
 import UserService from '../services/user.service'
 import boom from '@hapi/boom'
 
@@ -11,7 +11,19 @@ router.post('/', async (req, res, next) => {
     //TODO: Validate user data coming from the request
     const user: User = req.body
     const newUser = await service.create(user)
-    res.status(201).json({ user: newUser })
+    res.status(201).json({ user: newUser.toClient() })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { email } = req.query
+    const user = await service.findByEmail(email as string)
+    console.log({ user })
+
+    res.status(200).json({ user })
   } catch (error) {
     next(error)
   }
